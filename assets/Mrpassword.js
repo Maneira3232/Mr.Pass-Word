8414db201b0cec8e824d86cacf46a8b853f94c0d
Maneira3232 <jmaneira32@yahoo.com>
2021-11-06 11:19:49 -0400

    final Fixes

--- assets/Mrpassword.js
+++ assets/Mrpassword.js
@@ -5,10 +5,10 @@ var generatePasswordBtn = document.getElementById("generate");
 var specialCharStr = "!@#$%^&*()";
 var upperChaseCharLetterStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerChaseCharLetterStr = "abcdefghijklmnopqrstuvwxyz";
-var numberStr = "1234567890"
+var numberStr = "1234567890";
 
 
-
+// 
 function generatePassword(){
     var password = "";
 
@@ -21,26 +21,55 @@ function generatePassword(){
         
         return generatePassword();
     }
-
+//
     var passwordChars = "";
+
+    var passwordChars = prompt("Pick 1 to 10 Charaters")
+
+    if (passwordChars< 1 || passwordChars> 10){
+
+        alert("Only pick 1 to 10!!")
+
+        return generatePassword();
+    }
+
 
         passwordChars= passwordChars + upperChaseCharLetterStr;
         console.log(passwordChars);
 
-        //generate password here
-        
-        for(var i = 0; i < length; i++){
-            
-        }
+        passwordChars= passwordChars + lowerChaseCharLetterStr;
+        console.log(passwordChars)
+
+        passwordChars= passwordChars +
+        specialCharStr;
+        console.log(passwordChars)
+
+        passwordChars= passwordChars + numberStr;
+        console.log(passwordChars)
+
+
 
     
 
+   
+
+        //DONE
+        
+        for(var i = 0; i < length; i++){
+            var index = Math.floor(Math.random() * passwordChars.length)
+            console.log(passwordChars[index]);
+            console.log(password + " " + passwordChars[index]);
+
+           
+            password = password + passwordChars[index]
+
+        }
 
     return password;
 
 }
 
-//Main Process
+//DONE
 function writePassword(){
     var password = generatePassword();
     var passwordTextEl = document.getElementById("password");
@@ -54,4 +83,4 @@ function writePassword(){
 }
 
 
-generatePasswordBtn.addEventListener("click", writePassword)
+generatePasswordBtn.addEventListener("click", writePassword);
